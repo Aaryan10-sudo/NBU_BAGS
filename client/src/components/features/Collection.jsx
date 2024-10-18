ba164a6213d370c150b95cc92e5a0cda0c938569
Aarayn10-sudo <lazyfox916@gmail.com>
2024-10-18 12:04:20 +0545

    NBU

--- client/src/components/features/Collection.jsx
+++ client/src/components/features/Collection.jsx
@@ -10,12 +10,14 @@ const Collection = () => {
   const getAllProduct = async () => {
     try {
       let result = await axios({
-        url: "https://nbu-bags.onrender.com/readall",
+        url: "https://nbu-bags.onrender.com/product/read-all",
         method: "GET",
       });
       console.log(result?.data?.data);
       setProduct(result?.data?.data);
-    } catch (error) {}
+    } catch (error) {
+      console.log(error.message);
+    }
   };
   useEffect(() => {
     getAllProduct();
@@ -35,12 +37,14 @@ const Collection = () => {
             return (
               <div
                 key={index}
-                className="sm:w-[250px] w-[210px] h-[300px] shadow-lg sm:mt-[50px] mt-[20px] rounded-xl"
+                className="sm:w-[250px] w-[190px] h-[300px] shadow-lg sm:mt-[50px] mt-[20px] rounded-xl"
               >
                 <div className="h-[250px] bg-slate-400 rounded-tl-xl rounded-tr-xl"></div>
                 <div className="flex justify-between p-[10px]">
-                  <h1 className="font-bold text-[17px]">{value.productName}</h1>
-                  <span className="h-[30px] bg-blue-400 w-[90px] text-center font-bold text-white flex items-center justify-center rounded-md">
+                  <h1 className="font-bold sm:text-[17px] text-[17px]">
+                    {value.productName}
+                  </h1>
+                  <span className="h-[30px] bg-blue-400 sm:w-[90px] w-[70px] text-center font-bold text-white flex items-center justify-center rounded-md">
                     <p>{value.price}</p>
                   </span>
                 </div>
@@ -57,7 +61,7 @@ const Collection = () => {
       </div>
       <br />
       <br />
-      <div className="sm:mx-[50px] mx-[20px] sm:h-[250px] h-[200px] bg-[url('banner2.jpg')] bg-cover bg-center shadow-xl"></div>
+      <div className="sm:mx-[50px] mx-[20px] sm:h-[250px] h-[200px] bg-[url('/banner2.jpg')] bg-cover bg-center shadow-xl"></div>
       <br />
     </>
   );
