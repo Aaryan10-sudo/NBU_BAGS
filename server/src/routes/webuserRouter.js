f1ed110025a2f717c6bfebed54d5398e508a03b6
Aarayn10-sudo <lazyfox916@gmail.com>
2024-10-20 18:26:45 +0545

    NBU

--- server/src/routes/webuserRouter.js
+++ server/src/routes/webuserRouter.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   createWebuser,
   login,
+  totalWebuser,
   verifyMail,
 } from "../controller/webuserController.js";
 
@@ -9,5 +10,5 @@ let webuserRouter = Router();
 webuserRouter.route("/create").post(createWebuser);
 webuserRouter.route("/verify-mail").post(verifyMail);
 webuserRouter.route("/login").post(login);
-webuserRouter.route("/total-user").get(totalProduct);
+webuserRouter.route("/total-user").get(totalWebuser);
 export default webuserRouter;
