d69b74903b151d92908da88455d081f30509bda5
Aarayn10-sudo <lazyfox916@gmail.com>
2024-10-22 22:34:31 +0545

    NBU

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -17,30 +17,31 @@ const Product = () => {
   }, []);
   return (
     <div>
-      <h1 className="font-bold text-center text-[20px] my-[20px]">
+      <h1 className="font-bold text-center text-[25px] my-[20px] font-ubuntu">
         Discover Your Next Favorite{" "}
       </h1>
-      <div className="flex gap-[30px] sm:justify-normal  justify-between  flex-wrap md:mx-[40px] mx-[10px] my-[20px]">
+      <div className="flex md:justify-normal sm:justify-around justify-between  flex-wrap md:mx-[40px] mx-[10px] sm:gap-[30px] gap-[10px`] pb-[20px]">
         {product.map((value, index) => {
           return (
             <div
               key={index}
-              className="md:w-[210px] sm:w-[210px] w-[190px] h-[300px] shadow-lg md:mt-[0px] mt-[px] rounded-xl"
+              className="md:w-[210px] sm:w-[210px] w-[190px] h-auto shadow-lg md:mt-[30px] mt-[30px] "
             >
               <div className="h-[250px] rounded-tl-xl rounded-tr-xl object-cover object-center">
-                <img
-                  src={value.image}
-                  className="h-full w-full rounded-tr-xl rounded-tl-xl"
-                />
+                <img src={value.image} className="h-full w-full" />
               </div>
-              <div className="flex justify-between p-[10px]">
-                <h1 className="font-bold sm:text-[17px] text-[17px]">
-                  {value.productName}
-                </h1>
-                <span className="h-[30px] bg-blue-400 sm:w-[90px] w-[70px] text-center font-bold text-white flex items-center justify-center rounded-md">
-                  <p>{value.price}</p>
-                </span>
-              </div>
+              <span className="mx-[10px]">
+                <p className="w-[100px] bg-slate-400 mx-[10px] mt-[5px]">
+                  {value.category}
+                </p>
+              </span>
+
+              <h1 className="font-bold sm:text-[17px] text-[17px] mx-[10px] font-ubuntu">
+                {value.productName}
+              </h1>
+              <span className="h-[30px] sm:w-[90px] w-[70px] font-bold text-white rounded-md mx-[10px]">
+                <p className="mx-[10px] font-ubuntu">Rs. {value.price}</p>
+              </span>
             </div>
           );
         })}
