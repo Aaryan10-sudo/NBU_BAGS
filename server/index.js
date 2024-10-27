9971e4013757580bdea0329fc12df573a71df3a9
Aarayn10-sudo <lazyfox916@gmail.com>
2024-10-27 12:57:29 +0545

    server side index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import express, { json } from "express";
 import cors from "cors";
-import { connectDB } from "./src/connectDB/connectDB.js";
 import tokenRouter from "./src/routes/tokenRouter.js";
 import webuserRouter from "./src/routes/webuserRouter.js";
 import productRouter from "./src/routes/productRouter.js";
 import fileRouter from "./src/routes/fileRouter.js";
+import { connectDB } from "./src/connectDB.js";
 connectDB();
 let app = express();
 let port = process.env.PORT || 10000;
