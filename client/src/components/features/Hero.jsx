9616ffb6fb542ffef9a801c83e81af52e5460f22
Aarayn10-sudo <lazyfox916@gmail.com>
2024-10-27 12:57:49 +0545

    NBU

--- client/src/components/features/Hero.jsx
+++ client/src/components/features/Hero.jsx
@@ -3,8 +3,8 @@ import NavBar from "./Navbar";
 
 const Hero = () => {
   return (
-    <div className="bg-[url('/hero.jpg')] bg-cover bg-center md:h-[580px] sm:h-[450px] h-[330px] overflow-hidden flex object-cover">
-      <NavBar />
+    <div className="bg-[url('/hero.jpg')] bg-cover bg-center md:h-[580px] sm:h-[450px] h-[330px] flex object-cover">
+      <NavBar className={"sticky z-50"} />
     </div>
   );
 };
