2decced4c5d360a984eaa4034be9a3fad3c045fd
Aarayn10-sudo <lazyfox916@gmail.com>
2024-10-29 17:56:39 +0545

    Updated for wider screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import MyRoutes from "./routes/MyRoutes";
 
 function App() {
   return (
-    <div className="bg-gray-300">
+    <div className="bg-gray-300 max-w-[1280px] mx-auto">
       <MyRoutes />
     </div>
   );
