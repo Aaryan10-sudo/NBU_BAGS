b558f6341938448b377bb84ce24d7079df70d7e3
Aarayn10-sudo <lazyfox916@gmail.com>
2024-10-29 10:09:10 +0545

    Removed unnecessary import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,6 @@ import "./App.css";
 import MyRoutes from "./routes/MyRoutes";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="bg-gray-300">
       <MyRoutes />
