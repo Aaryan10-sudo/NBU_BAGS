9b4f5b09855dd26cd058abf7a9c5bc081018c252
Aarayn10-sudo <lazyfox916@gmail.com>
2024-10-29 19:46:33 +0545

    Loader added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,14 +28,14 @@ function App() {
   if (loading) {
     return (
       <div className="flex justify-center items-center h-screen bg-gray-300">
-        <div className="w-full gap-x-2 flex justify-center items-center">
-          <div className="w-7 bg-[#d991c2] animate-pulse h-7 rounded-full flex justify-center text-white items-center font-bold">
+        <div className="w-full gap-x-2 flex justify-center items-center animate-pulse">
+          <div className="w-7 bg-[#d991c2] animate-bounce h-7 rounded-full flex justify-center text-white items-center font-bold delay-0">
             N
           </div>
-          <div className="w-7 animate-pulse h-7 bg-[#9869b8] rounded-full flex justify-center text-white items-center font-bold">
+          <div className="w-7 h-7 bg-[#9869b8] animate-bounce rounded-full flex justify-center text-white items-center font-bold delay-700">
             B
           </div>
-          <div className="w-7 h-7 animate-pulse bg-[#6756cc] rounded-full flex justify-center text-white items-center font-bold">
+          <div className="w-7 h-7 bg-[#6756cc] animate-bounce rounded-full flex justify-center text-white items-center font-bold delay-1000">
             U
           </div>
         </div>
