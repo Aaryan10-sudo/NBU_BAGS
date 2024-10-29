2f0a86cd9094eeba4f5f8b56bbda52d411295e78
Aarayn10-sudo <lazyfox916@gmail.com>
2024-10-29 10:09:36 +0545

    Removed unnecessary import

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BsBackpack3 } from "react-icons/bs";
 import { FaShieldAlt, FaSmile } from "react-icons/fa";
 import { FaCouch, FaLeaf } from "react-icons/fa6";
-import Footer from "./features/Footer";
-import Hero from "./features/Hero";
 
 const About = () => {
   return (
