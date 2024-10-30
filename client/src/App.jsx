dc1cb4d53c1d53cbe4deec23b9fbe6ff82402ee4
Aarayn10-sudo <lazyfox916@gmail.com>
2024-10-30 14:03:21 +0545

    Loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,17 +28,7 @@ function App() {
   if (loading) {
     return (
       <div className="flex justify-center items-center h-screen bg-gray-300">
-        <div className="w-full gap-x-2 flex justify-center items-center animate-pulse">
-          <div className="w-7 bg-[#d991c2] animate-bounce h-7 rounded-full flex justify-center text-white items-center font-bold delay-0">
-            N
-          </div>
-          <div className="w-7 h-7 bg-[#9869b8] animate-bounce rounded-full flex justify-center text-white items-center font-bold delay-700">
-            B
-          </div>
-          <div className="w-7 h-7 bg-[#6756cc] animate-bounce rounded-full flex justify-center text-white items-center font-bold delay-1000">
-            U
-          </div>
-        </div>
+        <div class="loader"></div>
       </div>
     );
   }
